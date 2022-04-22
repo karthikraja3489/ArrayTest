0e1383b60f98ee1ecf9831885d53c1936cb59dd5
karthikraja3489 <rpckarthikraja@gmail.com>
2022-04-22 21:41:14 +0530

    new code added

---
+++ src/org/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package org.array;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		
+	int a[]= {1,2,3,4,5,6,7,8,9,10};
+	int sum=0;
+	float avg=0;
+	for(int i=0;i<a.length;i++) {
+		sum=sum+a[i];
+		avg=sum/a.length;
+	}
+	System.out.println("The average of numbers are "+avg);
+	}
+
+}
